30735f3f0b3692daad73729a1fb34078c070edcf
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2022-03-19 22:43:02 +0200

    use new hologram system

---
+++ lua/autorun/client/cl_convars.lua
@@ -0,0 +1,3 @@
+CreateClientConVar("seatholo_enabled", '1', true, false, "Enable Seat Holo?", 0, 1)
+CreateClientConVar("seatholo_no_outfitter", '0', true, false, "Don't use Outfitter playermodel?", 0, 1)
+CreateClientConVar("seatholo_flicker", '1', true, false, "Hologram flickers?", 0, 1)
