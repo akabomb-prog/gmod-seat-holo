a8e13a18eb9bb846edc384c928b7fb3af803b7dd
akabomb-prog <69766525+akabomb-prog@users.noreply.github.com>
2022-03-20 16:56:46 +0200

    add multiple hologram display
    add feature mentioned in issue #3

--- lua/autorun/client/cl_seatholo_context.lua
+++ lua/autorun/client/cl_seatholo_context.lua
@@ -1,14 +1,27 @@
 AddCSLuaFile()
 
-properties.Add("toggle_seat_holo", 
+properties.Add("enable_seat_holo", 
 {
-    MenuLabel = "Toggle Seat Holo",
-    Orde = 850,
+    MenuLabel = "Show seat hologram",
+    Order = 850,
     MenuIcon = "icon16/camera.png",
     Filter = function (self, ent, ply) 
-        return seat_holo.IsValidVehicle(ent)
+        return seat_holo.IsValidVehicle(ent) && !ent:GetVar("SeatHolo_forceHolo", false)
     end,
     Action = function (self, ent)
-        print(ent) -- test
+        ent:SetVar("SeatHolo_forceHolo", true)
+    end
+})
+
+properties.Add("disable_seat_holo", 
+{
+    MenuLabel = "Hide seat hologram",
+    Order = 850,
+    MenuIcon = "icon16/camera.png",
+    Filter = function (self, ent, ply) 
+        return seat_holo.IsValidVehicle(ent) && ent:GetVar("SeatHolo_forceHolo", false)
+    end,
+    Action = function (self, ent)
+        ent:SetVar("SeatHolo_forceHolo", false)
     end
 })
---
+++ lua/autorun/client/cl_seatholo_display_m.lua
@@ -0,0 +1,67 @@
+AddCSLuaFile()
+
+require("seat_holo")
+
+-- this is for displaying holograms on more vehicles
+
+hook.Add("Think", "SeatHolo_display_m", function ()
+    if !GetConVar("seatholo_enabled"):GetBool() then return end
+    
+    for i,vehicle in ipairs(ents.FindByClass("*vehicle*")) do
+        if seat_holo.IsValidVehicle(vehicle) then
+            local holo = vehicle:GetVar("SeatHolo_holo", NULL)
+            local shouldHolo = vehicle:GetVar("SeatHolo_forceHolo", false)
+            if !IsValid(holo) && shouldHolo then
+                -- create hologram
+                holo = ClientsideModel(LocalPlayer():GetModel(), RENDERGROUP_TRANSLUCENT)
+                holo:Spawn()
+            
+                -- model
+                if GetConVar("seatholo_no_outfitter"):GetBool() then
+                    holo:SetModel(player_manager.TranslatePlayerModel(GetConVar("cl_playermodel"):GetString())) -- cheap hack for now, it will very much lead to inconsistencies
+                else
+                    holo:SetModel(LocalPlayer():GetModel())
+                end
+            
+                -- color
+                local col = LocalPlayer():GetColor()
+                col.a = GetConVar("seatholo_alpha"):GetInt()
+                holo:SetColor(col)
+                holo:SetRenderMode(RENDERMODE_TRANSCOLOR)
+            
+                -- flicker effect
+                if GetConVar("seatholo_flicker"):GetBool() then
+                    holo:SetRenderFX(kRenderFxHologram)
+                else
+                    holo:SetRenderFX(kRenderFxNone)
+                end
+                
+                -- move to driver seat
+                local driverPos, driverAng = seat_holo.GetDriverSeat(vehicle)
+                holo:SetPos(driverPos)
+                holo:SetAngles(driverAng)
+            
+                -- parent hologram to vehicle
+                holo:SetParent(vehicle)
+                
+                if vehicle:GetVar("SeatHolo_sitSequence") != nil then
+                    -- if we already have a sequence, set
+                    holo:SetSequence(seat_holo.GetSitSequence(LocalPlayer(), vehicle))
+                else
+                    -- if we don't, set default
+                    holo:SetSequence("sit")
+                end
+
+                vehicle:SetVar("SeatHolo_holo", holo)
+
+                vehicle:CallOnRemove("RemoveSeatHolo", function (self)
+                    local holo = self:GetVar("SeatHolo_holo", NULL)
+                    if IsValid(holo) then holo:Remove() end
+                end)
+            else
+                vehicle:RemoveCallOnRemove("RemoveSeatHolo")
+                if IsValid(holo) then holo:Remove() end
+            end
+        end
+    end
+end)
